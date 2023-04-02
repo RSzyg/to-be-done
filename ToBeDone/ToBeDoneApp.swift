519ca14722e2d56727784af7759f4d682b639f17
RSzyh <whitezyg@gmail.com>
2023-04-02 12:13:38 +0800

    Initial Commit

---
+++ ToBeDone/ToBeDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToBeDoneApp.swift
+//  ToBeDone
+//
+//  Created by Zhang Yuhao on 2023/4/2.
+//
+
+import SwiftUI
+
+@main
+struct ToBeDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
